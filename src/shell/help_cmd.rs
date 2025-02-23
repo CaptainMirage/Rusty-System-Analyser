78be724c24a0b1838eed7b2d1083b6765b53d79a
Captain Mirage <chargersrthc6.2@gmail.com>
2025-02-23 12:59:50 +0330

    System Overhaul: adding help function #6.8

--- src/shell/help_cmd.rs
+++ src/shell/help_cmd.rs
@@ -27,6 +27,51 @@ lazy_static! {
                 description: "hey, you, yes you, if you can read this and understand it, \n\
                 then there is no need for an explanation of what this command does"
             });
+            m.insert("echo", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("type", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("pwd", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("drive-space", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("file-type-dist", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("largest-files", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("largest folder", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("recent-large-files", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("old-large-files", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("full-drive-analysis", CommandInfo {
+            title: "",
+            description: ""
+            });
+            m.insert("temp-694208008", CommandInfo {
+            title: "",
+            description: ""
+            });
+            
             m
         };
     }
